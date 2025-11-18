9b1db27a843c8ad520e911c054709e481178cccf
Devarsh Raj <devarshraj5@gmail.com>
2025-11-18 21:41:10 +0530

    prime no code #approach 1

--- DSAJourney/Level1_Basics/01_primenos.py
+++ DSAJourney/Level1_Basics/01_primenos.py
@@ -4,3 +4,19 @@ Problem:
     Given an integer n, determine whether it is a prime number.
     Return "YES" if prime, otherwise "NO".
 """
+
+# approach 1 - brute force
+
+def check_prime(n : int) -> False:
+    if n <=1:
+        return False
+    if n == 2:
+        return True
+
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+
+    return False
+
+
