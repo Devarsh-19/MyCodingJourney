7a095715b4d6c8a6d2ccc0892b940b4d199f324b
Devarsh Raj <devarshraj5@gmail.com>
2025-11-19 20:47:07 +0530

    prime no code #approach 2

--- DSAJourney/Level1_Basics/01_primenos.py
+++ DSAJourney/Level1_Basics/01_primenos.py
@@ -7,7 +7,7 @@ Problem:
 
 # approach 1 - brute force
 
-def check_prime(n : int) -> False:
+def check_prime_1(n : int) -> bool:
     if n <=1:
         return False
     if n == 2:
@@ -20,3 +20,20 @@ def check_prime(n : int) -> False:
     return False
 
 
+def check_prime_2(n: int) -> bool:
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+
+        # 2. Check for divisibility by 2 or 3
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+
+    return True
