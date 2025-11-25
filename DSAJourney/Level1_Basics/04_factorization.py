c7a65aab2ab9bcf7bc2232a4993be926a1aa4029
Devarsh Raj <devarshraj5@gmail.com>
2025-11-25 20:33:31 +0530

    prime factorization

--- DSAJourney/Level1_Basics/04_factorization.py
+++ DSAJourney/Level1_Basics/04_factorization.py
@@ -3,3 +3,31 @@
 Problem:
     Given a number n, return all of its prime factors in sorted order.
 """
+
+import math
+
+
+def get_prime_factors(n):
+    """
+    Returns all prime factors of n in sorted order.
+    Time Complexity: O(sqrt(n))
+    """
+    if n <= 1:
+        return []
+
+    factors = []
+
+    while n % 2 == 0:
+        factors.append(2)
+        n //= 2
+
+    i = 3
+    while i * i <= n:
+        while n % i == 0:
+            factors.append(i)
+            n //= i
+        i += 2
+    if n > 1:
+        factors.append(n)
+
+    return factors
