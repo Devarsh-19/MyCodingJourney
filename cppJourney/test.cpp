998e613c69bddcb41efd031b2fd71744ba0838d8
Devarsh Raj <devarshraj5@gmail.com>
2025-12-01 15:19:44 +0530

    cpp added 1 file

--- cppJourney/test.cpp
+++ cppJourney/test.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+using namespace std;
+
+
+int add(int x, int y){
+    return x + y;
+}
 
 int main(){
     printf("Hello world");
+
+    int a = 20, b =50;
+    cout << "SUM: " << add(a, b);
+
 }
 
 
