760670f0d50297ecf34478362b3d4f54ee65e41d
Devarsh Raj <devarshraj5@gmail.com>
2025-12-06 02:24:49 +0530

    amends

--- DSAJourney/Level1_Basics/02_gcd_lcm.py
+++ DSAJourney/Level1_Basics/02_gcd_lcm.py
@@ -1,8 +1,6 @@
-
 # 2.1. GCD
 # Problem:
-#     Given two integers a and b, compute their Greatest Common Divisor (GCD) using the Euclidean algorithm.
-
+# Given two integers a and b, compute their Greatest Common Divisor (GCD) using the Euclidean algorithm.
 def gcd_euclidean(a, b):
     a = abs(a)
     b = abs(b)
@@ -12,11 +10,9 @@ def gcd_euclidean(a, b):
     return a
 
 
-
 # 2.2. LCM
 # Problem:
-#     Given two integers a and b, find their Least Common Multiple (LCM) using LCM = (a*b) / GCD.
-
+# Given two integers a and b, find their Least Common Multiple (LCM) using LCM = (a*b) / GCD.
 def lcm(a, b):
     if a == 0 or b == 0:
         return 0
